d50d9535fe203c299e7697333e48854c8f17dccf
iampolly <aplap@list.ru>
2018-02-24 18:49:45 +0500

    Python-версия программы

--- lab0.py
+++ lab0.py
@@ -1,1 +1,9 @@
-print("hello!!");
+lst = [1, 1, 1, 1, 1, 1, 1]
+lst.sort()
+addlst = []
+for i in range(len(lst)-1):
+    r = lst[i+1]-lst[i]
+    if r > 1:
+        for j in range(r-1):
+            addlst.append(lst[i]+j+1)
+print(addlst)
